2983e92f18642e95d043e607bc7ac5ff314899f5
Eddo Dava <eddodava@gmail.com>
2025-04-08 08:55:40 +0700

    Refactor API routes to group authenticated endpoints and improve organization

--- routes/api.php
+++ routes/api.php
@@ -12,30 +12,30 @@ use App\Http\Controllers\MonevKeuanganController;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-
-Route::post('/register', [AuthController::class, 'register']);
+ // If you want to protect routes with authentication later, move them inside this group
 Route::post('/login', [AuthController::class, 'login']);
-
-Route::apiResource('programs', ProgramController::class);
-Route::get('pekerjaan/{pekerjaanId}/programs', [ProgramController::class, 'getByPekerjaanId']);
-
-// Pekerjaan routes
-Route::apiResource('pekerjaan', PekerjaanController::class);
-
-// Keuangan routes
-Route::apiResource('keuangan', KeuanganController::class);
-Route::get('pekerjaan/{pekerjaanId}/keuangan', [KeuanganController::class, 'getByPekerjaanId']);
-
-// JadwalProgram routes
-Route::apiResource('jadwal-program', JadwalProgramController::class);
-Route::get('program/{programId}/jadwal', [JadwalProgramController::class, 'getByProgramId']);
-
-// MonevKeuangan routes
-Route::apiResource('monev-keuangan', MonevKeuanganController::class);
-Route::get('pekerjaan/{pekerjaanId}/monev-keuangan', [MonevKeuanganController::class, 'getByPekerjaanId']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::get('/logout', [AuthController::class, 'logout']);
-    // If you want to protect routes with authentication later, move them inside this group
+   
+
+    Route::apiResource('programs', ProgramController::class);
+    Route::get('pekerjaan/{pekerjaanId}/programs', [ProgramController::class, 'getByPekerjaanId']);
+
+    // Pekerjaan routes
+    Route::apiResource('pekerjaan', PekerjaanController::class);
+
+    // Keuangan routes
+    Route::apiResource('keuangan', KeuanganController::class);
+    Route::get('pekerjaan/{pekerjaanId}/keuangan', [KeuanganController::class, 'getByPekerjaanId']);
+
+    // JadwalProgram routes
+    Route::apiResource('jadwal-program', JadwalProgramController::class);
+    Route::get('program/{programId}/jadwal', [JadwalProgramController::class, 'getByProgramId']);
+
+    // MonevKeuangan routes
+    Route::apiResource('monev-keuangan', MonevKeuanganController::class);
+    Route::get('pekerjaan/{pekerjaanId}/monev-keuangan', [MonevKeuanganController::class, 'getByPekerjaanId']);
+
 });
